62d383060b5b1701ab2738ac6960c91686c4320f
heypran <pranjal.baweja@gmail.com>
2024-05-05 20:45:22 +0200

    refactor: migrate home cards

--- src/components/home/HomeCard.tsx
+++ src/components/home/HomeCard.tsx
@@ -1,14 +1,13 @@
+import { OpenInNewWindowIcon } from '@radix-ui/react-icons';
 import {
-  Box,
-  Center,
-  Heading,
-  Text,
-  Stack,
-  useColorModeValue,
-  Link,
-  Divider,
-  Flex,
-} from '@chakra-ui/react';
+  CardTitle,
+  CardContent,
+  Card,
+  CardFooter,
+  CardHeader,
+} from '@shadcn-components/ui/card';
+import { Button } from '@shadcn-components/ui/button';
+import Link from 'next/link';
 
 export type HomeCardPropsType = {
   title: string;
@@ -22,45 +21,29 @@ export type HomeCardPropsType = {
 
 export function HomeCard(props: HomeCardPropsType) {
   return (
-    <Center>
-      <Box
-        w={'280px'}
-        h={'160px'}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'xl'}
-        rounded={'10px'}
-        p={4}
-        className={props.glow ? 'box glowing' : ''}
-      >
-        <Link
-          isExternal
-          aria-label={props.title}
-          href={props.isExternal ? `${props.link}` : `/${props.link}`}
-          style={{ textDecoration: 'none' }}
-          target="_blank"
-        >
-          <Flex flexDirection="column" height="100%">
-            <Stack>
-              <Heading
-                color={useColorModeValue('gray.700', 'white')}
-                fontSize={'xl'}
-                fontFamily={'body'}
-              >
-                {props.title}
-              </Heading>
-              <Divider my={2} />
-              <Text
-                color={'black'}
-                textAlign="justify"
-                noOfLines={4}
-                fontSize="sm"
-              >
-                {props.description}
-              </Text>
-            </Stack>
-          </Flex>
-        </Link>
-      </Box>
-    </Center>
+    <div className={props.glow ? 'box glowing' : ''}>
+      <Card>
+        <CardHeader>
+          <CardTitle>{props.title}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{props.description}</p>
+        </CardContent>
+        <CardFooter>
+          <Link
+            className="w-full"
+            aria-label={props.title}
+            href={props.isExternal ? `${props.link}` : `/${props.link}`}
+            style={{ textDecoration: 'none' }}
+            target="_blank"
+          >
+            <Button className="w-full">
+              Open &nbsp;
+              <OpenInNewWindowIcon className="mr-2 h-4 w-4" />
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
