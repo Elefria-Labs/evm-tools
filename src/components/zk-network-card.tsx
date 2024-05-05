bd9cf36f1f06527fd5db3dc37e334b367402aa72
heypran <pranjal.baweja@gmail.com>
2024-05-05 20:52:22 +0200

    refactor: migrate zknetwork cards

--- src/components/zk-network-card.tsx
+++ src/components/zk-network-card.tsx
@@ -1,15 +1,7 @@
-import {
-  Box,
-  Center,
-  Heading,
-  Text,
-  Stack,
-  useColorModeValue,
-  Divider,
-  Flex,
-  Button,
-} from '@chakra-ui/react';
+import { Button, Card, CardFooter, CardHeader } from '@chakra-ui/react';
+import { CardTitle, CardContent } from '@shadcn-components/ui/card';
 import { BlockchainNetwork } from '@types';
+import { CardStackPlusIcon } from '@radix-ui/react-icons';
 
 type ZkNetworkCardType = {
   onClickAdd: (network: number | undefined) => void;
@@ -18,57 +10,25 @@ type ZkNetworkCardType = {
 export function ZkNetworkCard(props: ZkNetworkCardType) {
   const { onClickAdd, blockchainNetwork } = props;
   return (
-    <Center>
-      <Box
-        maxW={'445px'}
-        minW={'320px'}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        p={6}
-        overflow={'hidden'}
-      >
-        <Flex
-          flexDirection="column"
-          justifyContent="space-between"
-          height="100%"
+    <Card>
+      <CardHeader>
+        <CardTitle> {blockchainNetwork.name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>{`Name: ${blockchainNetwork.name}`}</p>
+        <p>{`Chain Id: ${blockchainNetwork.chainId}`}</p>
+        <p>{`Currency: ${blockchainNetwork.nativeCurrency?.name} (${blockchainNetwork.nativeCurrency?.symbol})`}</p>
+        <p>{`Decimals: ${blockchainNetwork.nativeCurrency?.decimals}`}</p>
+      </CardContent>
+      <CardFooter>
+        <Button
+          className="w-full"
+          onClick={() => onClickAdd(blockchainNetwork.networkId)}
         >
-          <Stack>
-            <Heading
-              color={useColorModeValue('gray.700', 'white')}
-              fontSize={'2xl'}
-              fontFamily={'body'}
-            >
-              {blockchainNetwork.name}
-            </Heading>
-            <Divider my={4} />
-            <Text color={'black'} textAlign="justify" fontSize="sm">
-              {`Name: ${blockchainNetwork.name}`}
-            </Text>
-            <Text color={'black'} textAlign="justify" mt={2} fontSize="sm">
-              {`Chain Id: ${blockchainNetwork.chainId}`}
-            </Text>
-            <Text color={'black'} textAlign="justify" mt={2} fontSize="sm">
-              {`Currency: ${blockchainNetwork.nativeCurrency?.name} (${blockchainNetwork.nativeCurrency?.symbol})`}
-            </Text>
-            <Text color={'black'} textAlign="justify" mt={2} fontSize="sm">
-              {`Decimals: ${blockchainNetwork.nativeCurrency?.decimals}`}
-            </Text>
-          </Stack>
-
-          <Box mt={2}>
-            <Divider my={4} />
-            <Button
-              variant="solid"
-              onClick={() => {
-                onClickAdd(blockchainNetwork.networkId);
-              }}
-            >
-              Add Network
-            </Button>
-          </Box>
-        </Flex>
-      </Box>
-    </Center>
+          Add Network &nbsp;
+          <CardStackPlusIcon className="mr-2 h-4 w-4" />
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
