66988eee0eeecaec4b2f2d19665f7d71a91050b2
heypran <pranjal.baweja@gmail.com>
2024-06-10 20:09:34 +0200

    refactor: zk networks

--- src/pages/zk-networks.tsx
+++ src/pages/zk-networks.tsx
@@ -59,13 +59,6 @@ const ZkNetwork = () => {
                       <CardTitle>Current Network</CardTitle>
                     </CardHeader>
                     <CardContent>
-                      {/* {chainId && (
-                        <p>
-                          {`Network: ${
-                            networkConfig[toHex(chainId)]?.chainName
-                          }`}
-                        </p>
-                      )} */}
                       <p>{`Account: ${truncateAddress(account)}`}</p>
                       <p>{`Chain Id: ${chainId ?? 'No Network'}`}</p>
                     </CardContent>
@@ -84,7 +77,7 @@ const ZkNetwork = () => {
             </div>
             <div className="mb-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {Object.values(zkNetworks)
-                .filter((nt: BlockchainNetwork) => nt.isZk)
+                .filter((nt: BlockchainNetwork) => nt)
                 .map((network: BlockchainNetwork) => (
                   <ZkNetworkCard
                     key={network.name}
