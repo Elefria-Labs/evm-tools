218f98aaab7e45addf230bc0d508d99a8ffbf435
heypran <pranjal.baweja@gmail.com>
2024-06-26 15:39:50 +0200

    fix: gas convertor bugs

--- src/components/tools/evmtools/GasConvertorComponent.tsx
+++ src/components/tools/evmtools/GasConvertorComponent.tsx
@@ -5,18 +5,26 @@ import { useGetCoinPrice } from '@hooks/useGetCoinPrice';
 import { useGetBaseFee } from '@hooks/useGetBaseFee';
 import { Label } from '@shadcn-components/ui/label';
 import InputBaseCopy from '@components/common/BaseInputCopy';
+import { useToast } from '@chakra-ui/react';
+import { toastOptions } from '@components/common/toast';
 
 function calculateValue(ethAmount: string, gasPrice: string): string {
-  const ethValue = ethers.utils.parseEther(ethAmount.toString());
-  const value = ethValue.mul(ethers.utils.parseUnits(gasPrice.toString(), 18));
+  let value;
+  try {
+    const ethValue = ethers.utils.parseEther(ethAmount.toString());
+    value = ethValue.mul(ethers.utils.parseUnits(gasPrice.toString(), 18));
+  } catch (e) {
+    return ethers.utils.formatUnits(0, 18 * 2);
+  }
 
-  return ethers.utils.formatUnits(value, 18 * 2); // Convert the value to a human-readable format
+  return ethers.utils.formatUnits(value, 18 * 2);
 }
 
 export default function GasConverterComponent() {
   const [weiValue, setWeiValue] = useState<string>('');
   const [gweiValue, setGweiValue] = useState<string>('');
   const [ethValue, setEthValue] = useState<string>('');
+  const toast = useToast();
 
   const {
     data: ethPrice,
@@ -46,24 +54,38 @@ export default function GasConverterComponent() {
 
   const handleGweiChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const gweiValue = event.target.value;
-    if (!isValid(gweiValue)) {
+
+    setGweiValue(gweiValue);
+    try {
+      const weiValue = ethers.utils.parseUnits(gweiValue, 'gwei');
+      setWeiValue(weiValue.toString());
+      const ethValue = ethers.utils.formatEther(weiValue);
+      setEthValue(ethValue);
+    } catch (e) {
+      toast({
+        ...toastOptions,
+        title: 'Invalid input',
+      });
       return;
     }
-    setGweiValue(gweiValue);
-    const weiValue = ethers.utils.parseUnits(gweiValue, 'gwei');
-    setWeiValue(weiValue.toString());
-    const ethValue = ethers.utils.formatEther(weiValue);
-    setEthValue(ethValue);
   };
 
   const handleEthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const ethValue = event.target.value;
-    if (!isValid(ethValue)) {
+
+    setEthValue(ethValue);
+    let weiValue;
+    try {
+      weiValue = ethers.utils.parseEther(ethValue);
+      setWeiValue(weiValue.toString());
+    } catch (e) {
+      toast({
+        ...toastOptions,
+        title: 'Invalid input',
+      });
       return;
     }
-    setEthValue(ethValue);
-    const weiValue = ethers.utils.parseEther(ethValue);
-    setWeiValue(weiValue.toString());
+
     const gweiValue = ethers.utils.formatUnits(weiValue, 'gwei');
     setGweiValue(gweiValue);
   };
@@ -111,21 +133,6 @@ export default function GasConverterComponent() {
             onClick={() => handleCopyClick(ethValue)}
             onChange={handleEthChange}
           />
-          {/* <Input
-              type="text"
-              placeholder="ETH"
-              className="w-48"
-              // minWidth={[100, 400]}
-              value={ethValue}
-              onChange={handleEthChange}
-            />
-            <InputRightElement>
-              <IconButton
-                aria-label="Copy ETH Value"
-                icon={<CopyIcon />}
-                onClick={() => handleCopyClick(ethValue)}
-              />
-            </InputRightElement> */}
         </div>
       </div>
       {!!ethPrice?.[0] && (
