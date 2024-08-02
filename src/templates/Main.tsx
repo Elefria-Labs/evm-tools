f66be20181a1f222d6a5ea048da8b97dbd58cdba
heypran <pranjal.baweja@gmail.com>
2024-08-02 18:56:51 +0200

    feat: relevant tools

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -2,11 +2,14 @@ import { GlobalHeader } from '@components/global-header';
 import { PageWrapper } from '@components/page-wrapper';
 import { ReactNode } from 'react';
 import { Footer } from '@components/Footer';
+import ToolCarousel from '@components/common/ToolCarousel';
+import { playgroundToolKeyRecord, playgroundToolsList } from '@data/playground';
 
 type IMainProps = {
   meta: ReactNode;
   children: ReactNode;
   showConnectWallet?: boolean;
+  link?: string;
 };
 
 const Main = (props: IMainProps) => {
@@ -16,9 +19,25 @@ const Main = (props: IMainProps) => {
       <div style={{ paddingLeft: '0px', paddingTop: '0px' }}>
         <PageWrapper>
           <GlobalHeader showConnectWallet={props?.showConnectWallet} />
+          {/* <NavBar /> */}
           <div className="flex flex-col content-between items-center min-h-screen">
             <div className="flex flex-col max-w-[1024px] w-11/12">
               {props.children}
+            </div>
+          </div>
+          <div className="flex flex-col content-between items-center ">
+            <div className="flex flex-col content-between max-w-[1024px] w-11/12">
+              {props?.link != null && (
+                <ToolCarousel
+                  playgroundTools={playgroundToolsList.filter(
+                    (tool) =>
+                      tool.category ==
+                        // @ts-ignore
+                        playgroundToolKeyRecord[props.link!].category &&
+                      tool.link != props.link,
+                  )}
+                />
+              )}
             </div>
           </div>
           <Footer />
