8415a6d1a4af7b2b300d7fffcf0485483d46ae0c
heypran <pranjal.baweja@gmail.com>
2024-08-11 13:09:15 +0200

    fixes: contract ui

--- src/components/tools/evmtools/BaseContractUiComponent.tsx
+++ src/components/tools/evmtools/BaseContractUiComponent.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
+'use client';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { Input } from '@shadcn-components/ui/input';
-import { useChainId } from 'wagmi';
-import useGetContractAbi from '@hooks/useGetContractAbi';
+import { useChainId, usePublicClient } from 'wagmi';
+import { getContractAbi } from '@hooks/useGetContractAbi';
 import { Textarea } from '@shadcn-components/ui/textarea';
 import ContractUiComponent from './ContractUi/ContractUiComponent';
 import {
@@ -11,16 +12,25 @@ import {
   TabsTrigger,
 } from '@shadcn-components/ui/tabs';
 import { PlusCircledIcon, Cross1Icon } from '@radix-ui/react-icons';
-import { useGlobalStore } from '@store/global-store';
+import { ReadWriteUserContract } from '@store/state';
 
 const BaseContractUiComponent = () => {
-  const contracts = useGlobalStore.use.readWriteUserContracts();
-  const setContracts = useGlobalStore.use.setReadWriteUserContracts();
-
+  // const contracts = useGlobalStore.use.readWriteUserContracts();
+  // const setContracts = useGlobalStore.use.setReadWriteUserContracts();
+  const [contracts, setContracts] = useState<ReadWriteUserContract[]>([
+    {
+      id: '1',
+      address: '0xc3De830EA07524a0761646a6a4e4be0e114a3C83',
+      abi: '',
+      parsedAbi: null,
+      parseError: '',
+      contractAbi: null,
+      abiError: null,
+    },
+  ]);
   const [activeTab, setActiveTab] = useState('1');
   const chainId = useChainId();
-
-  const { getAbi, error: abiError, loading } = useGetContractAbi(chainId);
+  const publicClient = usePublicClient();
 
   const addNewContract = useCallback(() => {
     const newId = (
@@ -38,97 +48,128 @@ const BaseContractUiComponent = () => {
         abiError: null,
       },
     ]);
+    setActiveTab(newId);
+  }, [contracts, setContracts]);
 
-    setActiveTab((prevActiveTab) => (parseInt(prevActiveTab) + 1).toString());
-  }, []);
-
-  const closeContract = useCallback((id: string) => {
-    if (contracts.length > 1) {
-      const newContracts = contracts.filter((contract) => contract.id !== id);
-      setActiveTab(newContracts[0]!.id);
-      setContracts(newContracts);
-      return;
-    }
-  }, []);
+  const closeContract = useCallback(
+    (id: string) => {
+      if (contracts.length > 1) {
+        const newContracts = contracts.filter((contract) => contract.id !== id);
+        setContracts(newContracts);
+        setActiveTab(newContracts[0]!.id);
+      }
+    },
+    [contracts, setContracts],
+  );
 
   const updateContract = useCallback(
-    (id: string, field: string, value: string | any[] | null) => {
-      const newContracts = contracts.map((contract) => {
-        if (contract.id === id) {
-          if (field == 'address') {
-            // reset errors/abis in this case
-            return {
-              ...contract,
-              address: value as string,
-              abi: '',
-              parsedAbi: null,
-              parseError: '',
-              contractAbi: null,
-              abiError: null,
-            };
-          }
-          return { ...contract, [field]: value };
-        }
-        return contract;
-      });
-      setContracts(newContracts);
+    (id: string, updates: any) => {
+      setContracts(
+        contracts.map((contract) =>
+          contract.id === id ? { ...contract, ...updates } : contract,
+        ),
+      );
     },
-    [],
+    [contracts, setContracts],
+  );
+
+  const fetchAbi = useCallback(
+    async (contractId: string, address: string) => {
+      if (!address) return;
+      try {
+        const abi = await getContractAbi(address, chainId);
+        if (abi == null) {
+          updateContract(contractId, {
+            abiError:
+              'Could not fetch the ABI. Please provide raw ABI below in JSON format!',
+          });
+        } else {
+          updateContract(contractId, { contractAbi: abi, abiError: null });
+        }
+      } catch (error) {
+        updateContract(contractId, { abiError: (error as Error).message });
+      }
+    },
+    [chainId, updateContract],
   );
 
   useEffect(() => {
-    const fetchAbi = async () => {
-      const currentContract = contracts.find(
-        (contract) => contract.id === activeTab,
-      );
-      if (
-        currentContract &&
-        currentContract.address &&
-        currentContract.abi.length === 0 &&
-        !currentContract.contractAbi &&
-        !currentContract.abiError
-      ) {
-        try {
-          console.log('truing to get raonct ', currentContract);
-          const abi = await getAbi(currentContract.address);
-          updateContract(activeTab, 'contractAbi', abi);
-          updateContract(activeTab, 'abiError', null);
-        } catch (error) {
-          updateContract(activeTab, 'abiError', (error as Error).message);
-        }
-      }
-    };
+    const currentContract = contracts.find(
+      (contract) => contract.id === activeTab,
+    );
 
-    fetchAbi();
-  }, [activeTab, chainId, contracts, getAbi, updateContract]);
+    if (
+      currentContract &&
+      currentContract.address &&
+      currentContract.contractAbi == null &&
+      !currentContract.abiError &&
+      currentContract.abi.length === 0
+    ) {
+      fetchAbi(currentContract.id, currentContract.address);
+    }
+  }, [activeTab, contracts, fetchAbi]);
+
+  // useEffect(()=>{
+  //   //handle change in network
+  //   // remove all the abi
+  // },[chainId])
+
+  const handleAddressChange = useCallback(
+    (id: string, value: string) => {
+      updateContract(id, {
+        address: value,
+        abi: '',
+        parsedAbi: null,
+        parseError: '',
+        contractAbi: null,
+        abiError: null,
+      });
+    },
+    [updateContract],
+  );
 
   const handleAbiChange = useCallback(
     (id: string, value: string) => {
       try {
         const parsedAbi = JSON.parse(value);
-        updateContract(id, 'abi', value);
-        updateContract(id, 'parsedAbi', parsedAbi);
-        updateContract(id, 'parseError', '');
+        updateContract(id, {
+          abi: value,
+          parsedAbi,
+          parseError: '',
+        });
       } catch (e) {
-        updateContract(id, 'abi', value);
-        updateContract(id, 'parsedAbi', null);
-        updateContract(
-          id,
-          'parseError',
-          'Invalid ABI, please provide the ABI in the correct format!',
-        );
+        updateContract(id, {
+          abi: value,
+          parsedAbi: null,
+          parseError:
+            'Invalid ABI, please provide the ABI in the correct format!',
+        });
       }
     },
     [updateContract],
   );
 
+  const memoizedContracts = useMemo(() => contracts, [contracts]);
+
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Read-Write Contracts</h1>
+      <h1 className="text-2xl font-bold">
+        Read-Write Multiple Contracts{' '}
+        <span className="italic text-md">(*beta)</span>
+      </h1>
+      <p className="mt-2 mb-8 italic">
+        Use on-chain verified contracts or provide ABI. Anvil/Hardhat enabled
+        for local development.
+      </p>
+      <p className="mt-2 mb-8 italic text-green-500">
+        Please connect your wallet to auto fetch verified contracts. This is in
+        beta, report any bugs using Feedback.
+      </p>
+
       <Tabs value={activeTab} onValueChange={setActiveTab}>
         <div className="flex items-center">
           <TabsList>
-            {contracts.map((contract) => (
+            {memoizedContracts.map((contract) => (
               <TabsTrigger
                 key={contract.id}
                 value={contract.id}
@@ -150,21 +191,22 @@ const BaseContractUiComponent = () => {
             onClick={addNewContract}
           />
         </div>
-        {contracts.map((contract) => (
+        {memoizedContracts.map((contract) => (
           <TabsContent key={contract.id} value={contract.id}>
             <Input
               className="mb-4"
               value={contract.address}
-              onChange={(e) =>
-                updateContract(contract.id, 'address', e.target.value)
-              }
+              onChange={(e) => handleAddressChange(contract.id, e.target.value)}
               placeholder="Contract address"
             />
             {contract.abiError && (
               <div className="text-red-500 mb-4">{contract.abiError}</div>
             )}
             {contract.contractAbi ? (
-              <div>Contract ABI loaded successfully</div>
+              <div className="text-green-500 mb-4">
+                Contract ABI loaded successfully from{' '}
+                {publicClient?.chain?.name}
+              </div>
             ) : (
               <>
                 <Textarea
@@ -186,7 +228,7 @@ const BaseContractUiComponent = () => {
             {((contract.address && contract.contractAbi != null) ||
               contract.parsedAbi != null) && (
               <ContractUiComponent
-                //@ts-ignore
+                // @ts-ignore
                 abi={contract.contractAbi ?? contract.parsedAbi}
                 contractAddress={contract.address}
               />
@@ -198,4 +240,4 @@ const BaseContractUiComponent = () => {
   );
 };
 
-export default BaseContractUiComponent;
+export default React.memo(BaseContractUiComponent);
